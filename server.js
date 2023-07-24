2543ee07022df6bde308cb6abb51aa8325940b19
gimes28 <gimes28@gmail.com>
2023-07-24 18:58:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-    origin: "http://localhost:4200"
-  };
-  
+  origin: "http://localhost:4200"
+};
+
 app.use(cors());
 var userCollection = mongoose.connection.collection('Users');
 
@@ -20,42 +20,44 @@ app.get("/", async (req, res) => {
 
 app.get("/friends/:genre", async (req, res) => {
   console.log("Getting friends for a user based on genre " + req.params.genre);
-  const users = await userCollection.find( {"topGenre": req.params.genre} );
+  const users = await userCollection.find({ "topGenre": req.params.genre });
   const result = await users.toArray();
   console.log(result);
   return res.status(201).json(result);
 });
 
 app.post("/newuser", async (req, res) => {
-    console.log("Post for new user received");
-    console.log(req.body);
-    const updatedUser = await userCollection.findOneAndUpdate({"email": req.body.email}, {$set: {
+  console.log("Post for new user received");
+  console.log(req.body);
+  const updatedUser = await userCollection.findOneAndUpdate({ "email": req.body.email }, {
+    $set: {
       "displayName": req.body.displayName,
       "email": req.body.email,
       "imageUrl": req.body.imageUrl,
       "topGenre": req.body.topGenre,
-    }});
-
-    console.log(updatedUser);
-    if(!updatedUser.value) {
-      console.log("Creating new user");
-      const insertedUser = await userCollection.insertOne(req.body);
-      return res.status(201).json(insertedUser);
     }
-    return res.status(201).json(updatedUser);
   });
+
+  console.log(updatedUser);
+  if (!updatedUser.value) {
+    console.log("Creating new user");
+    const insertedUser = await userCollection.insertOne(req.body);
+    return res.status(201).json(insertedUser);
+  }
+  return res.status(201).json(updatedUser);
+});
 
 const PORT = process.env.PORT || 8080;
 const start = async () => {
-    try {
-      await mongoose.connect("INSERT CONNECTION STRING", {useNewUrlParser: true});
-      app.listen(PORT, () => console.log(`Server is running on port ${PORT}. Mongoose connection successful.`)
-      );
-      app.use(cors());
-    } catch (error) {
-      console.error(error);
-      process.exit(1);
-    }
-  };
-  
-  start();
+  try {
+    await mongoose.connect("mongodb+srv://admin-ayush:ayush-admin@cluster0.ptxlfeb.mongodb.net/Harmony?retryWrites=true&w=majority", { useNewUrlParser: true });
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}. Mongoose connection successful.`)
+    );
+    app.use(cors());
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+start();
